ad5253b95424fc803c28d48f098fe61264c62dc7
shivamsharma01 <shivamsharma.ade@gmail.com>
2018-05-04 11:25:16 +0530

    DI - p&cnamespace

---
+++ DI - p_&_c_namespace/src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import beans.Car;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("resources/spring.xml");
+		Car car = appContext.getBean("car", Car.class);
+		System.out.println(car);
+	}
+}
