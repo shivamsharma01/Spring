6b205de264f049763994a7ee7e8adefd4704f154
shivamsharma01 <shivamsharma.ade@gmail.com>
2018-05-04 11:05:52 +0530

    DI - 'depends-on' attribute

---
+++ DI - depends-on/src/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("resources/spring.xml");
+	}
+}
